90d3aacdac73059f0074a837181d5f48c2038423
Maxi-arch <mxmu025@gmail.com>
2024-09-24 15:42:14 +0300

    functionscontinued

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(param){
+    if (param !== undefined) {
+        return 'This Saturday, I want to bathe my dog!'
+        
+    } else { return 'This Saturday, I want to roller-skate!'
+        
+    }
+}
+function mondayWork(param){
+     if (param !== undefined) { return 'This Monday, I will work from home.'
+        
+     } else { return "This Monday, I will go to the office."
+        
+     }
+}
+function wrapAdjective(initial) {
+
+    if (initial ==='*') {return  function (adjective){
+        return `You are *${adjective}*!`;
+    }
+        
+    } else if(initial === '||'){return function (adjective){
+        return `You are ||${adjective}||!`
+    }}
+
+}
